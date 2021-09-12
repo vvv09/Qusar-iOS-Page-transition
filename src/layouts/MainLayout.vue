c907ee9325dbe4dbccc00b73994331624c71860e
Vadim Valunskii <dvvv09@gmail.com>
2021-09-12 14:03:59 +0300

    Initial commit: delete unusual authogenerated code; grab iOS-like header and footer from quasar.dev; add line awesome icons support; change primary color

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header bordered class="bg-white text-primary">
+      <q-toolbar>
+        <q-toolbar-title class="text-center">
+          Quasar Framework
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer bordered class="bg-white text-primary">
+      <q-tabs
+        no-caps
+        active-color="primary"
+        indicator-color="transparent"
+        class="text-grey"
+      >
+        <q-route-tab label="Home" icon="las la-home" />
+      </q-tabs>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+export default ({
+  name: 'MainLayout',
+})
+</script>
